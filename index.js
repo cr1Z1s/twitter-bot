2dafa8e5a59a8849a263641c6fd88b191c41ddba
cr1Z1s <yahia.chougui@gmail.com>
2022-05-15 14:04:57 +0100

    change module location of twit

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 var Twit = require('twit');
 const pool = require("./db");
+const twit = require("./twit")
 
-var T = new Twit({
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_KEY_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL: true,     // optional - requires SSL certificates to be valid.
-});
+// var T = new Twit({
+//   consumer_key: process.env.TWITTER_API_KEY,
+//   consumer_secret: process.env.TWITTER_API_KEY_SECRET,
+//   access_token: process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//   timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+//   strictSSL: true,     // optional - requires SSL certificates to be valid.
+// });
 
-T.get('search/tweets', { q: 'algeria since:2011-07-11', count: 5 }, function (err, data, response) {
+twit.get('search/tweets', { q: 'algeria since:2011-07-11', count: 5 }, function (err, data, response) {
   console.log(data);
 })
 
--- twit.js
+++ twit.js
@@ -1,6 +1,6 @@
 var Twit = require('twit');
 
-var T = new Twit({
+var twit = new Twit({
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_KEY_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
@@ -9,4 +9,4 @@ var T = new Twit({
   strictSSL: true,     // optional - requires SSL certificates to be valid.
 });
 
-
+module.exports = twit;

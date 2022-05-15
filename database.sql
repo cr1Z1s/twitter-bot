ee2d5c5694e1c9b7bd8cfc62a234a968d55c4aa7
cr1Z1s <yahia.chougui@gmail.com>
2022-05-15 16:02:42 +0100

    remove unnecessary files

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE cryptotweet;
 
-CREATE TABLE  tweets(
-  tweet_id VARCHAR(255) PRIMARY KEY,
-  screen_name VARCHAR(255),
-  date_created VARCHAR(255),
-  tweet_text TEXT,
+CREATE TABLE tweets(
+	tweet_id VARCHAR(255) PRIMARY KEY,
+	screen_name VARCHAR(255),
+	date_created VARCHAR(255),
+	tweet_text TEXT
 );
